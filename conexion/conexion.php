28f7054399c30f59f97973d526bf8dffd76b042a
LuisEnrique <46499209+LuisEML@users.noreply.github.com>
2022-08-05 16:42:11 -0500

    Update conexion.php

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -6,7 +6,7 @@
 
     // $con = @mysqli_connect($servidor, $usuario, $clave,$bdName);   
     $con = mysqli_init();
-    mysqli_ssl_set($con,NULL,NULL,"ssl/BaltimoreCyberTrustRoot.crt.pem",NULL,NULL);
+    mysqli_ssl_set($conn,NULL,NULL,"ssl/BaltimoreCyberTrustRoot.crt.pem",NULL,NULL);
     mysqli_real_connect($con, "database-utt.mysql.database.azure.com", "Administrador", "UTTservidor2022", "punto_venta", 3306, MYSQLI_CLIENT_SSL);
     
     
